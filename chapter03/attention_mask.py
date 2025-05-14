33a6dbc7279deb6c5307cf8ab05e1fce5430c413
JXY001A <jxy001a@gmail.com>
2025-05-14 09:04:07 +0800

    feat: keep learning

--- chapter03/attention_mask.py
+++ chapter03/attention_mask.py
@@ -34,4 +34,22 @@ mask_simple = torch.tril(torch.ones(context_length,context_length))
 # 相乘，就是相同位置的元素相乘，实现对角线以上元素为 0 的作用，从而实现掩码能力
 attn_weight_mask = attn_weights * mask_simple; 
 
-print('attn_weight_mask',attn_weight_mask)
+# print('attn_weight_mask',attn_weight_mask)
+
+# 对掩码后的权重重新归一化
+row_sums = attn_weight_mask.sum(dim=-1,keepdim=True)
+attn_weight_mask_normal = attn_weight_mask/row_sums
+
+# print(attn_weight_mask_normal,"attn_weight_mask_normal")
+
+
+# 优化注意力掩码实现
+
+# 生成一个对角线以上全是 1 的掩码矩阵
+mask  = torch.triu(torch.ones(context_length,context_length))
+# 将 mask 矩阵中所有为 True 的元素修改为 -torch.inf（负无穷大）
+attn_scores_masked = attn_scores.masked_fill(mask.bool(),-torch.inf)
+# print('attn_scores_masked',attn_scores_masked)
+# 重新归一化
+attn_scores_weight_masked = torch.softmax(attn_scores_masked/keys.shape[-1]**0.5,dim=1)
+print("attn_scores_weight_masked",attn_scores_weight_masked)
