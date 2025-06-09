9d92507954c67f8f30d50a0d39d66609c489158e
JXY001A <jxy001a@gmail.com>
2025-06-09 23:07:14 +0800

    feat: keep learning

--- chapter03/CausalAttention.py
+++ chapter03/CausalAttention.py
@@ -12,7 +12,9 @@ class CausalAttention(nn.Module):
         # 需的，但在这里具有一些优势。例如，当我们在大语言模型中使用 CausalAttention 类时，缓
         # 冲区会与模型一起自动移动到适当的设备（CPU 或 GPU），这在训练大语言模型时非常重要。这
         # 意味着我们无须手动确保这些张量与模型参数在同一设备上，从而避免了设备不匹配的错误
-        self.register_buffer('mask',torch.ones(context_length,context_length),diagonal=1)
+        # torch.triu 将矩阵对角线以上全部置为 1 的掩码矩阵
+        self.register_buffer('mask',torch.triu(torch.ones(context_length, context_length),diagonal=1)) 
+
     def forward(self,x):
         b,num_tokens,d_in = x.shape
         keys = self.W_key(x)
@@ -29,3 +31,25 @@ class CausalAttention(nn.Module):
         context_vec = attn_weights @ values
 
         return context_vec
+    
+inputs = torch.tensor(
+    [
+        [0.43, 0.15, 0.89],  # Your     (x^1)
+        [0.55, 0.87, 0.66],  # journey  (x^2)
+        [0.57, 0.85, 0.64],  # starts   (x^3)
+        [0.22, 0.58, 0.33],  # with     (x^4)
+        [0.77, 0.25, 0.10],  # one      (x^5)
+        [0.05, 0.80, 0.55],
+    ]  # step     (x^6)
+)
+d_in = inputs.shape[1]
+d_out = 2 
+batch = torch.stack((inputs, inputs), dim=0)
+# print(batch.shape) 
+# print(batch)
+torch.manual_seed(123)
+context_length = batch.shape[1]
+
+ca = CausalAttention(d_in, d_out, context_length, 0.0) 
+context_vecs = ca(batch)
+print("context_vecs.shape:", context_vecs.shape)
